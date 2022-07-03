678f55db31b5490599bc4673681d5dcedd25aa3c
marcocacciabue <64919062+marcocacciabue@users.noreply.github.com>
2022-07-03 19:33:31 -0300

    Update strategies.R

--- strategies.R
+++ strategies.R
@@ -4,40 +4,43 @@
 #
 
 ## Random effective strategy
-randeff <- function(val = sample(1:5, 1),...){
+randeff <- function(){
+  val = sample(1:5, 1)
   if(val>3) {
     2}
-  else {1}}
+  else {1}
+  }
 
 
 
 
 ## Random deffective strategy 
 ## Inverts Randeff
-randdeff <- function(...){
+randdeff <- function(){
   (tirada = randeff())
   if (tirada == 1)
-  {return(2)}
-  else{return(1)}}
+  {2}
+  else{1}
+  }
 
 
 
 ## Random basic strategy
-randbasic <- function(...){
+randbasic <- function(){
   sample(1:2,1)
 }
 
 
 
 ## Always deffective strategy
-alwdeff <- function(...){
+alwdeff <- function(){
   2
 }
 
 
 ## Always funtional strategy
 
-alwfunc <- function(...){
+alwfunc <- function(){
   1
 }
 
@@ -45,7 +48,7 @@ alwfunc <- function(...){
 
 ## Particle counter strategy
 
-count_def <- function(score,k,...){
+count_def <- function(score,k){
   
   if (score >= k*0.5) {
     2
